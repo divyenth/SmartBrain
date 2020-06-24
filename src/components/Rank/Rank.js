d40ca78a44e51b6390ff1574e87c3909536027e9
divyenth <divyenth2010@gmail.com>
2020-06-24 10:33:55 +0530

    pushing application to repo

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+//import './Rank.css';
+const Rank = () => {
+	return (
+		<div>
+			<div className='white f3'>
+			  {'Divyenth, your rank is ...'}
+			</div>
+			<div className='white f3'>
+			  {'#7'}
+			</div>
+		</div>
+	);
+}
+
+export default Rank;
