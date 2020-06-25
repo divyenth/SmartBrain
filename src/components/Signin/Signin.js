72ea6de37a5c4f00089af690598cc916673b0135
divyenth <divyenth2010@gmail.com>
2020-06-25 11:28:00 +0530

    front end done

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -4,15 +4,15 @@ const Signin = ({onRouteChange}) => {
 	return (
 		<article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 center shadow-5">
 			<main className="pa4 black-80">
-			  <form className="measure">
+			  <div className="measure">
 			    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
 			      <legend className="f1 fw6 ph0 mh0">Sign In</legend>
 			      <div className="mt3">
-			        <label className="db fw6 lh-copy f5" for="email-address">Email</label>
+			        <label className="db fw6 lh-copy f5" htmlFor="email-address">Email</label>
 			        <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
 			      </div>
 			      <div className="mv3">
-			        <label className="db fw6 lh-copy f5" for="password">Password</label>
+			        <label className="db fw6 lh-copy f5" htmlFor="password">Password</label>
 			        <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password"/>
 			      </div>
 			      <label className="pa0 ma0 lh-copy f6 pointer"><input type="checkbox"/> Remember me</label>
@@ -28,7 +28,7 @@ const Signin = ({onRouteChange}) => {
 			    <div className="lh-copy mt3">
 			      <p  onClick={() => onRouteChange('Register')} href="#0" className="f5 link black db grow pointer">Register</p>
 			    </div>
-			  </form>
+			  </div>
 			</main>
 		</article>
 	);
